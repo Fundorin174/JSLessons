6e865f0c58a4ed07ea7b6d52f551e28b81a135ee
Fundorin174 <Fundorin174@gmail.com>
2024-12-24 14:50:18 +0300

    start

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Product from "./Product";
+
 const Cart = () => {
 
   const selectedItems = useSelector(state => state.product.selected);
 
   return (<div>
-    <div className="products">
+    <h1>Корзина</h1>
+    {selectedItems && <div className="products">
       {selectedItems?.map((item) => <Product productItem={item} isCart={true} key={item.id} />)}
-    </div>
+    </div>}
+    <p>Количество товара: {selectedItems?.length || 0}</p>
   </div>);
 }
 
