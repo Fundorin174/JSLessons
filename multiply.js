1157341684920dc777372b89032a48d9d61d9f1a
Fundorin174 <Fundorin174@gmail.com>
2025-01-24 14:36:08 +0300

    RequireJS

--- multiply.js
+++
@@ -1,9 +0,0 @@
-var multiply = function(numbers) {
-  var result = 1;
-  for (var i = 0; i < numbers.length; i++) {
-    result *= numbers[i];
-  }
-  return result;
-};
-
-var result = multiply(numbers);
